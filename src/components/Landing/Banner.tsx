9be198438e10a9e601b0d3d90a539b3d593c5890
Abeeb Maroof <abeebdon@gmail.com>
2025-07-04 23:38:19 +0100

    Ang mobile responsiveness

--- src/components/Landing/Banner.tsx
+++ src/components/Landing/Banner.tsx
@@ -1,75 +1,50 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import HeroSlider from "../user/HeroSlider";
 
 const Banner = () => {
   return (
-    <section className="bg-[#111111] pt-11 flex flex-col items-center">
-      <div className="w-full max-w-[84px] h-[95px] ">
-        <Image
-          width={0}
-          height={0}
-          alt="logo"
-          src={"/logo.svg"}
-          style={{ width: "100%", height: "100%" }}
-          sizes="100vw"
-        />
-      </div>
+    <>
+      <section className="bg-[#111111] pt-11 p-5 md:p-0 flex flex-col items-center">
+        <div className="w-full max-w-[84px] h-[95px] ">
+          <Image
+            width={0}
+            height={0}
+            alt="logo"
+            src={"/logo.svg"}
+            style={{ width: "100%", height: "100%" }}
+            sizes="100vw"
+          />
+        </div>
 
-      <h1 className="mt-10 text-center justify-start text-primaryBrown text-6xl font-normal font-['Abril_Fatface'] leading-[72.32px]">
-        Over 45 Years of
-        <br />
-        Design Innovation
-      </h1>
-      <p className="mt-[15px] w-[861px] text-center justify-start">
-        <span className="text-primaryBrown text-lg font-normal font-['Poppins'] leading-7">
-          KELLY HOPPEN INTERIORS{" "}
-        </span>
-        <span className="text-primaryBrown text-lg font-normal font-['Poppins'] lowercase leading-7">
-          IS DEDICATED TO DELIVERING MULTI-FACETED COUTURE PROJECTS TO THE
-          HIGHEST LEVELS OF DESIGN, OFFERING A COMPLETELY PERSONALIZED SERVICE
-          TO EACH CLIENT AROUND THE WORLD.
-        </span>
-      </p>
-      <Link href={"/signup"}>
-        <button className="cursor-pointer mt-[15px] w-56 p-2.5 bg-primaryBrown hover:bg-transparent text-[#111111] hover:text-primaryBrown  border border-primaryBrown  rounded-[10px] inline-flex justify-center items-center gap-2.5 overflow-hidden">
-          <span className="text-center justify-start  text-base font-semibold font-['Montserrat'] leading-tight">
-            Get Started
+        <h1 className="mt-10 text-center  justify-start text-primaryBrown text-[40px] leading-[130%] md:text-6xl font-normal font-['Abril_Fatface'] md:leading-[72.32px]">
+          Over 45 Years of
+          <br />
+          Design Innovation
+        </h1>
+        <p className="mt-[15px]  w-full max-w-[861px] text-center justify-start">
+          <span className="text-primaryBrown text-[14px] md:text-lg font-normal font-['Poppins'] leading-6  md:leading-7">
+            KELLY HOPPEN INTERIORS{" "}
           </span>
-        </button>
-      </Link>
-
-      <div
-        className="relative h-[654px] max-w-[1256px] w-full mt-[27px] border-2 "
-        style={{
-          backgroundImage: "url(/images/banner.png)",
-          backgroundSize: "cover",
-        }}
-      >
-        <div className="absolute top-0 left-0 w-full h-full bg-[#00000033] flex flex-col items-center justify-end pb-[68px]">
-          <button className="w-fit px-10 py-3.5 bg-primaryBrown rounded-[10px] inline-flex justify-center items-center gap-2.5">
-            <span className="text-center justify-start text-[#111111] text-4xl font-normal font-['Abril_Fatface'] leading-10">
-              Classic
+          <span className="text-primaryBrown text-lg font-normal font-['Poppins'] lowercase leading-7">
+            IS DEDICATED TO DELIVERING MULTI-FACETED COUTURE PROJECTS TO THE
+            HIGHEST LEVELS OF DESIGN, OFFERING A COMPLETELY PERSONALIZED SERVICE
+            TO EACH CLIENT AROUND THE WORLD.
+          </span>
+        </p>
+        <Link href={"/signup"}>
+          <button className="cursor-pointer mt-[15px] w-56 p-2.5 bg-primaryBrown hover:bg-transparent text-[#111111] hover:text-primaryBrown  border border-primaryBrown  rounded-[10px] inline-flex justify-center items-center gap-2.5 overflow-hidden">
+            <span className="text-center justify-start  text-base font-semibold font-['Montserrat'] leading-tight">
+              Get Started
             </span>
           </button>
-
-          <div className="mt-[140px] flex gap-11 items-center">
-            <button className="inline-flex bg-[#111111] py-3 px-3.5 rounded-[10px] text-white ">
-              <FaChevronLeft />
-            </button>
-            <div className="flex gap-4">
-              {Array.from({ length: 5 }, (_, index) => (
-                <div className="w-7 h-7 bg-[#111111] rounded-full" />
-              ))}
-            </div>
-            <button className="inline-flex bg-primaryBrown py-3 px-3.5 rounded-[10px] text-white ">
-              <FaChevronRight />
-            </button>
-          </div>
-        </div>
-      </div>
-    </section>
+        </Link>
+      </section>
+      <article className="p-5 bg-[#111111] w-full md:pb-0 mx-auto">
+        <HeroSlider mainClassName="!h-[390px] md:!h-[653px] max-w-[1256px]" />
+      </article>
+    </>
   );
 };
 
--- src/components/Landing/ProvidingInnovativeDesign.tsx
+++ src/components/Landing/ProvidingInnovativeDesign.tsx
@@ -4,18 +4,18 @@ import React from "react";
 const ProvidingInnovativeDesign = () => {
   return (
     <section className="bg-[#111111] py-20">
-      <h2 className="max-w-max mx-auto text-right justify-start text-stone-400 text-7xl font-normal font-['Abril_Fatface'] leading-[95.12px]">
+      <h2 className="max-w-max mx-auto text-right justify-start text-stone-400 text-[40px] md:text-7xl font-normal font-['Abril_Fatface'] leading-[130%] md:leading-[95.12px]">
         Providing Innovative
         <br />
         Interior Design.
       </h2>
 
-      <div className="relative flex justify-end mt-[90px]">
-        <div className="absolute left-0 top-0 bg-white w-full max-w-[50%] h-[600px]"></div>
-        <div className="relative mt-[190px] w-full min-w-[958px] max-w-[60%] min-h-[578px] bg-[#111] pt-[155px]">
-          <div className="absolute w-full max-w-[90%] -left-[47px] top-[10px]">
-            <div className="absolute -top-6 -left-6 bg-[#111] h-[440px] w-[40%]"></div>
-            <div className="w-full h-[428px] relative z-[2] ">
+      <div className="relative flex justify-end mt-5 md:mt-[90px]">
+        <div className="absolute left-0 top-0 bg-white w-full max-w-[50%] h-[252px] md:h-[600px]"></div>
+        <div className="relative mt-[190px] w-[60%] h-[360px] md:h-[578px] bg-[#111] pt-[80px] md:pt-[155px]">
+          <div className="absolute w-full md:max-w-[90%] -left-[47px] top-[10px]">
+            <div className="absolute -top-6 -left-6 bg-[#111]  h-[200px] md:h-[440px] w-1/2 md:w-[40%]"></div>
+            <div className="w-full h-[183px] md:h-[428px] relative z-[2] ">
               <Image
                 width={0}
                 height={0}
@@ -27,12 +27,16 @@ const ProvidingInnovativeDesign = () => {
               />
             </div>
           </div>
-          <div className="w-full h-[578px] rounded-tr-[70%]  bg-primaryBrown  p-[60px] flex flex-col gap-10 justify-end">
-            <p className="max-w-[697px] justify-start text-black text-xl font-normal font-['Plus_Jakarta_Sans'] leading-loose">
+          <div className="  w-full h-[325px] md:h-[620px] rounded-tr-[70%]  bg-primaryBrown  md:p-[60px] p-4 md:pt-[100px] flex flex-col gap-10 justify-end">
+            <p className="max-w-[697px] justify-start hidden md:block pt-[50px] text-black text-sm md:text-xl font-normal font-['Plus_Jakarta_Sans'] leading-loose">
               Since 1999, we have been providing great flooring solutions and
               customer service for homeowners and commercial clients. among
               flooring materials, none is more elegant and luxurious than
               natural stone.
+            </p>
+            <p className="max-w-[697px] md:hidden justify-start pt-[50px] text-black text-sm md:text-xl font-normal font-['Plus_Jakarta_Sans'] leading-loose">
+              We are known for our solutions in interior designs over the years
+              and our customers know us well.
             </p>
             <div className="max-w-[697px] flex justify-between">
               <button className="w-fit px-7 py-4 bg-neutral-900 inline-flex justify-center items-center gap-2.5 overflow-hidden">
@@ -40,7 +44,7 @@ const ProvidingInnovativeDesign = () => {
                   About Us
                 </span>
               </button>
-              <div className="w-full max-w-[94px] ">
+              <div className="w-full hidden md:block  max-w-[94px] ">
                 <Image
                   width={0}
                   height={0}
